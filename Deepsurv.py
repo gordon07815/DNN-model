f63ed0c25d9e6bc72498af98cf5978e92b562470
gordon07815 <55423263+gordon07815@users.noreply.github.com>
2022-04-29 11:18:18 +0800

    Delete Deepsurv.py

--- Deepsurv.py
+++
@@ -1,5 +0,0 @@
-#https://humboldt-wi.github.io/blog/research/information_systems_1920/group2_survivalanalysis/
-#https://github.com/jaredleekatzman/DeepSurv
-!git clone https://github.com/jaredleekatzman/DeepSurv.git
-!cd DeepSurv
-!pip install .
